fc49484f726ea7edcb74bd422341a92218f8dc65
Carol Vilchez <design.harls@icloud.com>
2025-08-21 11:22:35 +0200

    Imagenes incorporadas

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// lista de  ingredientes 
+const ingredientes = [
+    {
+        imagenSrc: 'assets/img/aguacate.webp', name: 'Aguacate'
+    },
+    {
+        imagenSrc: 'assets/img/brocoli.webp', name: 'Brocoli'
+    },
+     {
+        imagenSrc: 'assets/img/carne.webp', name: 'Carne'
+    },
+     {
+        imagenSrc: 'assets/img/cebolla.webp', name: 'Cebolla'
+    },
+     {
+        imagenSrc: 'assets/img/coliflor.webp', name: 'Coliflor'
+    },
+     {
+        imagenSrc: 'assets/img/patatas.webp', name: 'Patatas'
+    },
+     {
+        imagenSrc: 'assets/img/pescado.webp', name: 'Pescado'
+    },
+     {
+        imagenSrc: 'assets/img/pimientos.webp', name: 'Pimientos'
+    },
+     {
+        imagenSrc: 'assets/img/queso.webp', name: 'Queso'
+    },
+     {
+        imagenSrc: 'assets/img/tomate.webp', name: 'Tomate'
+    },
+    
+];
